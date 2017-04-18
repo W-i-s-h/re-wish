b6ab946a7ab0b49c28ccd50b7488a8acacc345bd
W-i-s-h <w-i-s-h@list.ru>
2017-04-18 18:27:39 +0300

    Full remake

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,79 +2,151 @@ package sample;
 
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 import jssc.SerialPort;
 import jssc.SerialPortEvent;
 import jssc.SerialPortEventListener;
 import jssc.SerialPortException;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 public class Controller {
-    public Button com;
-    public SerialPort serialPort;
-    public Button com1;
-    public Label lab1;
-    public String a;
 
-    public void letsCOM() {
 
-                SerialPort serialPort = new SerialPort("/dev/ttyS0");
-                try {
-                    serialPort.openPort();
-                    serialPort.setParams(SerialPort.BAUDRATE_9600, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
-//                    byte[] buffer = serialPort.readBytes(2);
-                    a = serialPort.readString(1);
-                    serialPort.closePort();
-                    System.out.println(a);
-                    System.out.println("Хей, получилось");
-                }
-                catch (SerialPortException ex) {
-                    System.out.println(ex);
-                }
-            }
 
-    public void letsData() {
-        lab1.setText("Данные: " + a);
-    }
+    public SerialPort serialPort;                     /*—Создаем объект типа SerialPort*/
+    public String a;                                  /*создаем стоковую переменную для храния значения*/
+    public Button butt1;
+    public Button butt11;
+    public Label labdata;
+    byte[] buffer;
+
+
+    public void letsCOM() throws SerialPortException {                          /*метод открывающий порт*/
+
+        try {
+            serialPort = new SerialPort("COM3");                              /*ставим интересующий нас порт*/
+//            serialPort = new SerialPort("/dev/cu.usbmodem1431");                    /*или этот. В зависимости от системы*/
+            serialPort.openPort();                                                    /*открываем порт*/
+            serialPort.setParams(9600, 8, 1, 0);    /*после открытия порта обязательно указываем параметры*/
+            labdata.setText("Порт открыт успешно");
+        } catch (SerialPortException e) {
+            e.printStackTrace();
+            labdata.setText("Порт не открыт");
         }
 
-//        SerialPort serialPort = new SerialPort("COM6");
-//        try {
-//            serialPort.openPort();
-//            serialPort.setParams(SerialPort.BAUDRATE_9600, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
-//            String a = "test";
-//            serialPort.writeBytes(a.getBytes());
+    }
+
+    public void letsSend() throws SerialPortException {                     /*метод ждущий принимающий 10 байт*/
+        try {
+//            buffer = serialPort.readBytes(2);
+            a = serialPort.readString(10);                        /*читаем данные из порта в созданную ранее переменную*/
 //            serialPort.closePort();
-//            System.out.println(a);
-//        }
-//        catch (SerialPortException ex) {
-//            System.out.println(ex);
-//        }
-//    }
-//}
-//        serialPort = new SerialPort("COM3");
+            System.out.println(a);
+            labdata.setText("Данные приняты успешно");
+        } catch (SerialPortException ex) {
+            System.out.println(ex);
+            labdata.setText("Данные неприняты");
+//            serialPort.closePort();
+        }
+    }
+
+    public void letsClose() throws SerialPortException {                     /*метод закрывающий порт*/
+        try {
+            serialPort.closePort();
+            labdata.setText("Порт закрыт успешно");
+        } catch (SerialPortException e) {
+            e.printStackTrace();
+            labdata.setText("Порт или уже закрыт или не закрыт");
+        }
+
+    }
+
+    public void letsDate() {                                                /*отображаем данные в поле Данные: */
+        labdata.setText("Данные: " + a);
+    }
+
+
+
+//      Предыдущие неудачне попытки, но надо все разобрать. Потому что все должно работать
+
+//        String name ="Test";
+//        byte[] a = name.getBytes();
+//        System.out.println(name.getBytes());
+//        System.out.println(a);
+//
+//        String str = new String(a);
+//        System.out.println(str);
+
+//        SerialPort serialPort = new SerialPort("/dev/ttyS0");
 //        try {
 //            serialPort.openPort();
 //            serialPort.setParams(SerialPort.BAUDRATE_9600, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
-//            serialPort.setEventsMask(SerialPort.MASK_RXCHAR);
-//            serialPort.addEventListener(new EventListener());
+//            serialPort.writeBytes("A".getBytes());
+//
+//
+//            byte[] buffer = serialPort.readBytes(2);
+//
+//            FileWriter fw1 = new FileWriter("/home/pi/Desktop/AGOT.txt", true);
+//            fw1.write(buffer.toString());
+//            fw1.append(buffer.toString());
+//            fw1.flush();
+//
+//            System.out.println(buffer);
+//            serialPort.closePort();
 //        }
 //        catch (SerialPortException ex) {
 //            System.out.println(ex);
 //        }
 //    }
+
+
+//        openPort();
+//        serialPort.writeBytes("A".getBytes());
+//        serialPort.setEventsMask(SerialPort.MASK_RXCHAR); /*Устанавливаем маску или список события на которые будет происходить реакция. В данном случае это приход данных в буффер порта*/
+//        serialPort.addEventListener(new EventListener()); /*Передаем экземпляр класса EventListener порту, где будет обрабатываться события. Ниже описан класс*/
 //
-//    private class EventListener implements SerialPortEventListener {
+//        }
 //
+//
+//    public void openPort() throws SerialPortException {
+//        serialPort = new SerialPort("/dev/ttys000");
+//        serialPort.openPort();
+//        serialPort.setParams(SerialPort.BAUDRATE_9600, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
+//    }
+//
+//    public void portClose() throws SerialPortException {
+//        serialPort.closePort();
+//    }
+//
+//    public class EventListener implements SerialPortEventListener { /*Слушатель срабатывающий по появлению данных на COM-порте*/
 //        public void serialEvent(SerialPortEvent event) {
-//            if(event.isRXCHAR() && event.getEventValue() == 8){
+//            if (event.isRXCHAR() && event.getEventValue() > 0) { /*Если происходит событие установленной маски и количество байтов в буфере более 0*/
 //                try {
-//                    byte[] buffer = serialPort.readBytes(8);
+//                    data = serialPort.readString(event.getEventValue()); /*Создаем строковую переменную  data, куда и сохраняем данные*/
+//                    try (FileWriter fw1 = new FileWriter("/home/pi/Desktop/test.txt", true)) {
+//                        String text = data;
+//                        fw1.write(text);
+//                        fw1.append('\n');
+//
+//                        fw1.flush();
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
+//                    System.out.print(data);/*Выводим данные на консоль*/
+//                    System.out.println("Вывожу данные на экран " + data);
 //                    serialPort.closePort();
-//                    System.out.println(buffer);
-//                }
-//                catch (SerialPortException ex) {
+//                } catch (SerialPortException ex) {
 //                    System.out.println(ex);
 //                }
 //            }
 //        }
 //    }
-//}
+
+    public void exit() {
+        Stage stage = (Stage)butt11.getScene().getWindow();
+        stage.close();
+    }
+}
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,12 +8,24 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    Stage window1;
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
+        window1 = primaryStage;
+        primaryStage.setOnCloseRequest(event -> windowClose());
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 305));
+        primaryStage.setScene(new Scene(root, 230, 600));
+        primaryStage.setResizable(false);                                                   /*Никаких растягивающися и развернутых окон*/
+        primaryStage.setFullScreen(false);
+        primaryStage.setMaximized(false);
         primaryStage.show();
+
+    }
+
+    private void windowClose() {
+        window1.close();
     }
 
 
